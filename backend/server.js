335240715e2b99134e1fe3f5d75491ef0968c216
kavidu717 <dushmanthakavidu143@gmail.com>
2026-05-20 21:25:20 +0530

    set up mongo db url

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import connectDB from './src/config/db.js'
 
 
 
 
 const app = express()
 
+
+app.use(cors())
+app.use(express.json())
+dotenv.config()
+
+
+connectDB()
 
 app.get('/', (req, res) => {
     res.send('backend running kaviduheloo with docker hello neda kavidu')
