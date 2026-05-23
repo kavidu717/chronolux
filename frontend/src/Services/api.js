4e57d414cd85e5b479bc6166c55af93ac1a1b767
kavidu717 <dushmanthakavidu143@gmail.com>
2026-05-23 16:39:30 +0530

    update backend url

--- frontend/src/Services/api.js
+++ frontend/src/Services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 
  const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://chronolux-1rge.onrender.com/api",
 });
 
 export default api
